6c4909faa5f2dacb62f8115884a45c82c8f1f579
oscar <oscar@HYDRA010.CADIT.spain>
2020-03-09 12:48:52 +0100

    crear

---
+++ 06_ejemplo_springboot/src/main/java/com/curso/controllers/SaludoController.java
@@ -0,0 +1,15 @@
+package com.curso.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SaludoController {
+
+	@GetMapping(value="/saludo", produces=MediaType.TEXT_PLAIN_VALUE)
+	public String getSaludo() {
+		
+		return "Bienvenido a Spring BOOT";
+	}
+}
